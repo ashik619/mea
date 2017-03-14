5a3d4f8de2dcb5f04db7429af645946cca003a0e
ashik619 <ashik5119@gmail.com>
2017-03-14 00:22:26 -0700

    check if user exist added

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -5,17 +5,24 @@ mongoose.set('debug', true);
 var User = require('../models/user_model.js');
 
 router.post('/create', function(req, res) {
-	var temp = new User();
-	temp.name = req.body.name;
-	temp.phoneNumber = req.body.phone_number;
-	temp.playerId = req.body.player_id
-	temp.save(function(err){
-		if(err){
-			return res.json({ success: false , 
-				message: 'Error!' });
+	User.findOne({phoneNumber : req.body.phone_number},function(err, user){
+		if(user){
+			res.json({success: false, msg : 1});
+		}else{
+			var temp = new User();
+			temp.name = req.body.name;
+			temp.phoneNumber = req.body.phone_number;
+			temp.playerId = req.body.player_id
+			temp.save(function(err){
+				if(err){
+					return res.json({ success: false , 
+						message: 'Error!' });
+				}
+			});
+			res.json({ success: true , msg: 0 });
+			
 		}
-	});
-	res.json({ success: true , message: 'User created!' });
+		});
 	
 });
 
